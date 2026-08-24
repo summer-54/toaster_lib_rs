f490e52b1c5c3513c0feb25ac077247786a76038
Yankovskiy Dmitriy <dmityank@gmail.com>
2026-08-24 16:02:47 +0300

    added S generic into grpc::Stream

--- src/server/grpc/mod.rs
+++ src/server/grpc/mod.rs
@@ -6,6 +6,7 @@ pub mod pb {
     tonic::include_proto!("invoker_manager");
 }
 
+use futures::StreamExt;
 pub use pb::invoker_manager_service_client::InvokerManagerServiceClient as Client;
 pub use pb::invoker_manager_service_server::{
     InvokerManagerService as Service, InvokerManagerServiceServer as Server,
@@ -13,18 +14,15 @@ pub use pb::invoker_manager_service_server::{
 
 use crate::prelude::*;
 
-use tokio::sync::{
-    Mutex,
-    mpsc::{UnboundedReceiver, UnboundedSender},
-};
+use tokio::sync::{Mutex, mpsc::UnboundedSender};
 
-pub struct Stream<I, O> {
-    receiver: Mutex<UnboundedReceiver<I>>,
+pub struct Stream<I, O, S: futures::Stream<Item = I>> {
+    receiver: Mutex<S>,
     sender: UnboundedSender<O>,
 }
 
-impl<I, O> Stream<I, O> {
-    pub fn new(receiver: UnboundedReceiver<I>, sender: UnboundedSender<O>) -> Self {
+impl<I, O, S: futures::Stream<Item = I>> Stream<I, O, S> {
+    pub fn new(receiver: S, sender: UnboundedSender<O>) -> Self {
         Self {
             receiver: Mutex::new(receiver),
             sender,
@@ -32,18 +30,19 @@ impl<I, O> Stream<I, O> {
     }
 }
 
-impl<I, O, SI, SO> super::stream::Stream<I, O> for Stream<SI, SO>
+impl<I, O, SI, SO, S> super::stream::Stream<I, O> for Stream<SI, SO, S>
 where
     SI: TryInto<I, Error = Error> + Send,
     O: Into<SO> + Send,
     SO: Send + Sync + 'static,
+    S: futures::Stream<Item = SI> + Unpin + Send,
 {
     fn recv(&self) -> impl Future<Output = anyhow::Result<anyhow::Result<I>>> + Send {
         async {
             self.receiver
                 .lock()
                 .await
-                .recv()
+                .next()
                 .await
                 .context("receiving message")
                 .map(|msg| msg.try_into().context("converting message"))
