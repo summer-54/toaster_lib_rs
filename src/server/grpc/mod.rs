d45d94fa16e4a30ad8eda09c5cfb32de9e45383c
Yankovskiy Dmitriy <dmityank@gmail.com>
2026-08-24 16:25:19 +0300

    sf

--- src/server/grpc/mod.rs
+++ src/server/grpc/mod.rs
@@ -11,17 +11,18 @@ pub use pb::invoker_manager_service_client::InvokerManagerServiceClient as Clien
 pub use pb::invoker_manager_service_server::{
     InvokerManagerService as Service, InvokerManagerServiceServer as Server,
 };
+use tonic::Status;
 
 use crate::prelude::*;
 
 use tokio::sync::{Mutex, mpsc::UnboundedSender};
 
-pub struct Stream<I, O, S: futures::Stream<Item = I>> {
+pub struct Stream<I, O, S: futures::Stream<Item = Result<I, Status>>> {
     receiver: Mutex<S>,
     sender: UnboundedSender<O>,
 }
 
-impl<I, O, S: futures::Stream<Item = I>> Stream<I, O, S> {
+impl<I, O, S: futures::Stream<Item = Result<I, Status>>> Stream<I, O, S> {
     pub fn new(receiver: S, sender: UnboundedSender<O>) -> Self {
         Self {
             receiver: Mutex::new(receiver),
@@ -35,21 +36,22 @@ where
     SI: TryInto<I, Error = Error> + Send,
     O: Into<SO> + Send,
     SO: Send + Sync + 'static,
-    S: futures::Stream<Item = SI> + Unpin + Send,
+    S: futures::Stream<Item = Result<SI, Status>> + Unpin + Send,
 {
-    fn recv(&self) -> impl Future<Output = anyhow::Result<anyhow::Result<I>>> + Send {
-        async {
-            self.receiver
-                .lock()
-                .await
-                .next()
-                .await
-                .context("receiving message")
-                .map(|msg| msg.try_into().context("converting message"))
-        }
+    async fn recv(&self) -> anyhow::Result<anyhow::Result<I>> {
+        Ok(self
+            .receiver
+            .lock()
+            .await
+            .next()
+            .await
+            .context("receiving message")?
+            .context("reading message")?
+            .try_into()
+            .context("converting message"))
     }
 
-    fn send(&self, msg: O) -> impl Future<Output = anyhow::Result<()>> + Send {
-        async { self.sender.send(msg.into()).context("sending message") }
+    async fn send(&self, msg: O) -> anyhow::Result<()> {
+        self.sender.send(msg.into()).context("sending message")
     }
 }
